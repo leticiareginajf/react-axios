1f5212356f53391d67f9e2f8bd8dfb88ae8f8d69
leticiareginajf <leticia.reginajf@gmail.com>
2025-07-27 17:38:08 -0300

    atualizacao

--- react-axios/src/main.jsx
+++ react-axios/src/main.jsx
@@ -1,30 +1,32 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-import {creteBrowserRouter, RouterProvider, Route} from 'react-router-dom'
+import {createBrowserRouter, RouterProvider, Route} from 'react-router-dom'
 
 import Home from './routes/Home.jsx'
 import NewPost from './routes/NewPost.jsx'
 
-const route = creteBrowserRouter([
+const router = createBrowserRouter([
   {
     element: <App />,
     children: [
       {
         path: "/",
-        element: <Home />,
+        element: <Home />
       },
       {
         path: "/new",
-        element: <NewPost />,
+        element: <NewPost />
       },
     ]
   }
 ])
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </StrictMode>,
 )
